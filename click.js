111dca28dfa9dad05f92ee7e63c75fdaa36f8b96
LetsStudyTogether <1050361636@qq.com>
2019-01-07 23:40:02 +0800

    现在可以自动切换到当前学期

--- click.js
+++ click.js
@@ -2,10 +2,12 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function clickonit() {
+var sleeptime=3500;
+
+async function clickonit(sleeptime) {
     $("a[href*='eva']").click();
     var array;
-    await sleep(3000);
+    await sleep(sleeptime);
     array = $("input[value^='评教']");
     if (array.length > 0) {
         $("input[value^='评教']")[0].click()
@@ -14,7 +16,7 @@ async function clickonit() {
         alert('Finish!');
         return;
     }
-    await sleep(3000);
+    await sleep(sleeptime);
     $("input[onclick*='8.8']").click();
     $("input[onclick*='9.6']").click();
     jQuery("#flag").val(true);
@@ -23,8 +25,14 @@ async function clickonit() {
     $("#saveId").hide();
     $("#sumbitId").hide();
     $("#btnWait").show();
-    await sleep(3000);
+    await sleep(sleeptime);
     clickonit();
 }
 
+$("a[href*='eva']").click();
+await sleep(sleeptime);
+$("td[val=42]").click();
+$("td[index=19]").click();
+$("input[type=submit]").click();
+await sleep(sleeptime);
 clickonit();
